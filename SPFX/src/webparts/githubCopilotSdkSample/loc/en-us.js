5622cbd75907b87831cca106e251c4c683f6660e
Tobias Maestrini <tobias.maestrini@gmail.com>
2026-01-25 13:50:51 +0100

    fix: update BasicGroupName to provide a more descriptive label

--- SPFX/src/webparts/githubCopilotSdkSample/loc/en-us.js
+++ SPFX/src/webparts/githubCopilotSdkSample/loc/en-us.js
@@ -1,7 +1,7 @@
 define([], function() {
   return {
     "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
+    "BasicGroupName": "Backend API (endpoint)",
     "DescriptionFieldLabel": "Description Field",
     "AppLocalEnvironmentSharePoint": "The app is running on your local environment as SharePoint web part",
     "AppLocalEnvironmentTeams": "The app is running on your local environment as Microsoft Teams app",
